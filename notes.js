570c172c946bf75d495604818a667c7047a0d3ad
Deathworld <corentin.g@gmail.com>
2018-05-29 11:40:20 +0000

    Finished Notes Node

--- notes.js
+++ notes.js
@@ -1,6 +1,42 @@
-console.log('Starting Note.js');
+console.log('Starting Notes.js');
+const fs = require('fs');
 
-module.exports.addNote = function() {
-    console.log('addNote');
-    return 'new note';
+
+var addNote = (title, body) => {
+    console.log('Adding note entitled : ' + title);
+    console.log("Here's the note's content : " + body);
+
+    var notes = ["", ""];
+    var note = {
+      title: title,
+      body: body
+    };
+
+    var notesString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(notesString);
+
+    notes = notes.push(note);
+
+    fs.writeFileSync(`notes-data.json`, JSON.stringify(notes))
+
+};
+
+var getAll = () => {
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting the note entitled : ' + title);
+    console.log("Here's the note's content : " + body);
+};
+
+var removeNote = (title) => {
+    console.log('Successfully deleted the note entitled : ' + title);
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 };
